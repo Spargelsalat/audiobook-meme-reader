1548e5fe9b573513008896ea7804e9291adcd1d2
Spargelsalat <alexander.wolf1720@gmail.com>
2025-01-01 03:33:54 +0100

    Adjustments

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -3,6 +3,10 @@ class AppConstants{
   const AppConstants._();
 
   static const String appName = "Audiobook Meme Reader";
-  static const String logFileName = "app_logs.log";
+  static const String logFile = "app_logs.log";
+  static const String appVersion = "0.0.1";
+  static const String logDir = "logs";
   static const String DefaultLanguage = "de";
+  static const String audioDir = 'audio';
+  static const String cachedir = 'cache';
 }
