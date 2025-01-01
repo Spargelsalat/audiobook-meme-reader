2d5bc668729e6ea902592fb844980365db61352d
Spargelsalat <alexander.wolf1720@gmail.com>
2025-01-01 03:15:44 +0100

    app_constants created and rudimentary Information

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,8 @@
+class AppConstants{
+  //Contruktor als private deklarieren und somit verhindern, dass die Klasse instanziiert wird von außen
+  const AppConstants._();
+
+  static const String appName = "Audiobook Meme Reader";
+  static const String logFileName = "app_logs.log";
+  static const String DefaultLanguage = "de";
+}
